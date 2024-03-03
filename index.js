03b81946047e4c4998ab51f9d64dca75b403055b
Aymane Touzani <158565484+gxaymenofc@users.noreply.github.com>
2024-03-03 13:27:33 +0100

    🧿 HARDY - BOT 🧿
    🧿 HARDY - BOT 🧿

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('The Mystic\nBot', {
+say('Hardy\nBot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`Bot creado por Bruno Sobrino`, {
+say(`Powered by Aymane Touzani`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});
